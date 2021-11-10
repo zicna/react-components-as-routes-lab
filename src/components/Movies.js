66aad4dc11e4ba7910cbca9e5e1d6ef1ace63a01
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-11-10 13:25:59 -0600

    commit tree 9b2cd60cfb695b593dd1bfa8a0cf1f56874abad9

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { movies } from '../data';
 
+
+
 const Movies = () => {
+  
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+          <div key={index}>
+            {movie.title}
+            {movie.time}
+            <ul>
+              {movie.genres.map(genre=>{
+                return (
+                  <li>{genre}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      )}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      <NavLink exact to='/'>Home</NavLink>
+      <NavLink exact to='/movies'>Movies</NavLink>
+      <NavLink exact to='/directors'>Directors</NavLink>
+      <NavLink exact to='/actors'>Actors</NavLink>
     </div>
   );
 };
